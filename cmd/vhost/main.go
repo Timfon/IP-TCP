198a153b639f82bf21d4f39a2563b61ecd1d9988
“Timfon” <timothy_fong@brown.edu>
2024-10-02 18:36:52 -0400

    need to figure out udp

--- cmd/vhost/main.go
+++ cmd/vhost/main.go
@@ -1,11 +1,41 @@
 package main
 import (
   "fmt"
-  "IP/pkg/protocol"
+  //"IP/pkg/protocol"
+  "os"
+	"IP/pkg/lnxconfig"
+  "net"
+  "net/netip"
 )
 func main() {
-  fmt.Println("Hello, vhost!")
-  protocol.PrintProtocol()
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage:  %s --config <lnx file>\n", os.Args[0])
+		os.Exit(1)
+	}
+	fileName := os.Args[2]
+	lnxConfig, err := lnxconfig.ParseConfig(fileName)
+	if err != nil {
+		panic(err)
+	}
+  udpPort, err := netip.ParseAddrPort(lnxConfig.Interfaces[0].UDPAddr.String())
+
+  fmt.Printf("UDP: %s\n", udpPort.String())
+  bindLocalAddr, err := net.ResolveUDPAddr("udp4", udpPort.String())
+  if err != nil {
+    fmt.Printf("Error resolving UDP address: %s\n", err)
+  }
+  conn, err := net.ListenUDP("udp4", bindLocalAddr)
+  if err != nil {
+    fmt.Printf("Error listening on UDP address: %s\n", err)
+  }
+  defer conn.Close()
+
+  // ONE CALL TO WriteToUDP => 1 PACKET
+  bytesWritten, err := conn.WriteToUDP([]byte("Hello, vhost-> vrouter"), bindLocalAddr)
+  if err != nil {
+    fmt.Printf("Error writing to UDP address: %s\n", err)
+  }
+  fmt.Printf("Wrote %d bytes to UDP address\n", bytesWritten)
 }
 
 
--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"IP/pkg/lnxconfig"
+	"log"
+	"net"
 	"net/netip"
-  "net"
+  "IP/pkg/lnxconfig"
 	"os"
 )
 
@@ -15,36 +16,56 @@ func main() {
 	}
 	fileName := os.Args[2]
 
-	// Parse the file
 	lnxConfig, err := lnxconfig.ParseConfig(fileName)
 	if err != nil {
 		panic(err)
 	}
 
-	// Demo:  print out the IP for each interface in this config
+	// Goroutine for each interface
 	for _, iface := range lnxConfig.Interfaces {
 		prefixForm := netip.PrefixFrom(iface.AssignedIP, iface.AssignedPrefix.Bits())
 		fmt.Printf("%s has IP %s\n", iface.Name, prefixForm.String())
-    udpPort, err := netip.ParseAddrPort(iface.UDPAddr.String())
-    if err != nil {
-      fmt.Printf("Error parsing UDP address: %s\n", err)
-    }
-    fmt.Printf("UDP: %s\n", udpPort.String())
-    bindLocalAddr, err := net.ResolveUDPAddr("udp4", udpPort.String())
-    if err != nil {
-      fmt.Printf("Error resolving UDP address: %s\n", err)
-    }
-    conn, err := net.ListenUDP("udp4", bindLocalAddr)
-    if err != nil {
-      fmt.Printf("Error listening on UDP address: %s\n", err)
-    }
-    bytesWritten, err := conn.WriteToUDP([]byte("Hello, world!"), bindLocalAddr)
-    if err != nil {
-      fmt.Printf("Error writing to UDP address: %s\n", err)
-    }
-    fmt.Printf("Wrote %d bytes to UDP address\n", bytesWritten)
+
+		udpPort, err := netip.ParseAddrPort(iface.UDPAddr.String())
+		if err != nil {
+			fmt.Printf("Error parsing UDP address: %s\n", err)
+			continue
+		}
+		fmt.Printf("UDP: %s\n", udpPort.String())
+
+		bindLocalAddr, err := net.ResolveUDPAddr("udp4", udpPort.String())
+		if err != nil {
+			fmt.Printf("Error resolving UDP address: %s\n", err)
+			continue
+		}
+
+		conn, err := net.ListenUDP("udp4", bindLocalAddr)
+		if err != nil {
+			fmt.Printf("Error listening on UDP address: %s\n", err)
+			continue
+		}
+		// Handle each UDP connection in a separate goroutine
+		go handleUDPConnection(conn)
 	}
-	//fmt.Printf("\n\nFull config:  %+v\n", lnxConfig)
+  //prevents main from exiting
+	select {}
 }
 
+// handleUDPConnection manages the UDP connection in a separate goroutine
+func handleUDPConnection(conn *net.UDPConn) {
+	defer conn.Close()
 
+	buffer := make([]byte, 1024)
+	for {
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Printf("Error reading from UDP connection: %s\n", err)
+			return
+		}
+		fmt.Printf("Received %s from %s\n", string(buffer[:n]), addr)
+		_, err = conn.WriteToUDP([]byte("Hello, vhost-> vrouter"), addr)
+		if err != nil {
+			fmt.Printf("Error writing to UDP address: %s\n", err)
+		}
+	}
+}
